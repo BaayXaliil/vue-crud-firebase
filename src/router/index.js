0d36138621d3015147416212fa98fe1c04a1800e
BayeKhalil <ibrahima.diop@idev-ic.net>
2021-11-07 10:40:35 +0000

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 Vue.use(VueRouter)
 
@@ -8,15 +10,50 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/new',
+    name: 'new-employee',
+    component: () => import('../views/NewEmployee.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/:employer_id',
+    name: 'view-employee',
+    component: () => import('../views/ViewEmployee.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/edit/:employer_id',
+    name: 'edit-employee',
+    component: () => import('../views/EditEmployee.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue'),
+    meta: {
+      requiresGuest: true
+    }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/Register.vue'),
+    meta: {
+      requiresGuest: true
+    }
   }
 ]
 
@@ -25,5 +62,42 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+// Nav Guard
+router.beforeEach((to, from, next) => {
+  // Check for requiresAuth guard
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // Check if NO logged user
+    if (!firebase.auth().currentUser) {
+      // Go to login
+      next({
+        name: 'login',
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      // Proceed to route
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    // Check if NO logged user
+    if (firebase.auth().currentUser) {
+      // Go to login
+      next({
+        name: 'Home',
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      // Proceed to route
+      next();
+    }
+  } else {
+    // Proceed to route
+    next();
+  }
+});
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <ul class="collection with-header">
+      <li class="collection-header"><h4>Employers</h4></li>
+      <li
+        v-for="employer in employers"
+        :key="employer.id"
+        class="collection-item"
+      >
+        <div class="chip">{{ employer.departement }}</div>
+        {{ employer.employer_id }}: {{ employer.nom }}
+        <router-link
+          class="secondary-content"
+          :to="{
+            name: 'view-employee',
+            params: { employer_id: employer.employer_id },
+          }"
+          ><i class="material-icons">visibility</i
+        ></router-link>
+      </li>
+    </ul>
+    <div class="fixed-action-btn">
+      <router-link to="/new" class="btn-floating btn-large red">
+        <i class="material-icons">add</i>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import db from "../components/firebaseConfig";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      employers: [],
+      loading: true,
+    };
+  },
+  created() {
+    db.collection("employers")
+      .orderBy("position")
+      .get()
+      .then((querySnapshot) => {
+        this.loading = false;
+        querySnapshot.forEach((doc) => {
+          const data = {
+            id: doc.id,
+            employer_id: doc.data().employer_id,
+            nom: doc.data().nom,
+            departement: doc.data().departement,
+            position: doc.data().position,
+          };
+          this.employers.push(data);
+        });
+      });
+  },
+};
 </script>
